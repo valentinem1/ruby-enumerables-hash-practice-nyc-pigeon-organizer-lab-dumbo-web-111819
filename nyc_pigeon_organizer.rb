f5a664dd21517574f27b4df33d45f7b582731b5c
Valentine Maillard <valentinemaillard1@gmail.com>
2019-11-06 19:14:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,44 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final = {}
+
+  data.each do |first_level, all_other|
+    all_other.each do |category, array|
+      array.each do |name|
+        final[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end
+  pigeon_names = final.keys
+  data[:color].each do |color, name|
+    name.each do |each_name|
+      pigeon_names.each do |bird_name|
+        if each_name == bird_name
+      final[bird_name][:color] << color.to_s
+        end
+      end
+    end
+  end
+  data[:gender].each do |gender, name|
+    name.each do |each_name|
+    pigeon_names.each do |bird_name|
+      if each_name == bird_name
+      final[bird_name][:gender] << gender.to_s
+      end
+    end
+  end
+ end
+   data[:lives].each do |location, name|
+    name.each do |bird_name|
+      pigeon_names.each do |item|
+        if bird_name === item
+          final[item][:lives] << location
+          binding.pry
+        end 
+      end 
+    end 
+  end 
+  
+  return final
 end
